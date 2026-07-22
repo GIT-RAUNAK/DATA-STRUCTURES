c9f9d883755c985952b29eede006b78234739038
GIT-RAUNAK <raunak2808.business@gmail.com>
2026-07-22 23:34:31 +0530

    Print Linked List

---
+++ 6. Linked List/P1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    Node* head = NULL;
+
+    cout << "Enter the elements: ";
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        Node* newNode = new Node(x);
+
+        if (head == NULL) {
+            head = newNode;
+        } else {
+            Node* temp = head;
+            while (temp->next != NULL) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    vector<int> result;
+
+    Node* temp = head;
+    while (temp != NULL) {
+        result.push_back(temp->data);
+        temp = temp->next;
+    }
+
+    cout << "Linked List Elements: ";
+    for (int x : result) {
+        cout << x << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
